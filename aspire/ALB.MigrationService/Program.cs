41bbe0df09964f49ed80f2e04e1573c5cc3fc863
TheBenda <andre.benda@protonmail.com>
2026-04-17 17:50:25 +0200

    Refactor service defaults to `HostServiceDefaults`, update dependencies, and enhance End-to-End tests

--- aspire/ALB.MigrationService/Program.cs
+++ aspire/ALB.MigrationService/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddNpgsqlDataSource("postgresdb",
     configureDataSourceBuilder: sourceBuilder => sourceBuilder.UseNodaTime());
 
-builder.AddServiceDefaults();
+builder.AddHostServiceDefaults();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
